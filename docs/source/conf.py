ef9cf6f2744158190f85b12df13602a18588a17a
Matthias Werner <Matthias.Werner1@tu-dresden.de>
2020-05-04 13:36:37 +0200

    docs: fixes logo location.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,8 +60,9 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-html_logo = "../images/alpaka.svg"
+#html_static_path = ['_static']
+
+html_logo = "../logo/alpaka.svg"
 html_theme_options = {
     "logo_only"  : True
 }
