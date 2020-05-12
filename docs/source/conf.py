7e2f8087ecdc36d7d8cc6894890e4fcbe9e8b1b7
Matthias Werner <Matthias.Werner1@tu-dresden.de>
2020-05-12 14:24:21 +0200

    Fixes sphinx latexpdf issue.
    - logo/alpaka.pdf collided with alpaka.pdf returned from alpaka.tex,
      so renamed latex master file to alpaka-doc.tex (-> alpaka-doc.pdf)
      (sphinx copies files into a flat hierarchy, so name conflicts are
      happening)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -95,12 +95,13 @@ latex_elements = {
     #
     # 'figure_align': 'htbp',
 }
+latex_logo = "../logo/alpaka.pdf"
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'alpaka.tex', u'alpaka Documentation',
+    (master_doc, 'alpaka-doc.tex', u'alpaka Documentation',
      u'The alpaka Community', 'manual'),
 ]
 
